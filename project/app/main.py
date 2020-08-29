f8ff2eb5e560cf9b504b733f65c892c056fcf7d4
danijel <danijel.kecman@gmail.com>
2020-08-29 21:02:29 +0200

    - add basic routes and crud - deploy to heroku

--- project/app/main.py
+++ project/app/main.py
@@ -6,7 +6,7 @@ import logging
 from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
 
-from app.api import ping
+from app.api import ping, summaries
 from app.db import init_db
 
 
@@ -16,6 +16,7 @@ log = logging.getLogger(__name__)
 def create_application() -> FastAPI:
     application = FastAPI()
     application.include_router(ping.router)
+    application.include_router(summaries.router, prefix='/summaries', tags=['summaries'])
 
     return application
 
